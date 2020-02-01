58a3b2a39196093b2dba3d151728c319d7641c59
med661 <60036955+med661@users.noreply.github.com>
2020-02-01 14:26:06 +0100

    CSS CHECKPOINT

---
+++ htmlcss/main.js
@@ -0,0 +1,39 @@
+console.log("it's the begining");
+/*alert("hhhh");*/
+/*var x=5;
+var y=6;
+var z=x+y;
+alert(z);*/
+/*
+var String="i love ";
+var p="pizza";
+alert(String.length);
+console.log("it's the "+"pizza");*/
+/*var string="I LOVE PIZZA";
+alert(string.toLowerCase())*/
+/*var second="i love pizza";
+alert(second.toUpperCase());*/
+/*var a=5;
+var b=6;
+alert(a==b);*/
+console.log(2==5||6==6);
+/*var i;
+for(i=0;i<10;i++)
+{
+    alert(i);
+}*/
+/*var i=0;
+while(i<10){
+    alert(i);
+    i++;
+}*/
+/*var myarray=[1,2,3,4];
+var saarray=["1","2","3"];
+console.log(myarray);
+console.log(saarray);
+myarray.push(6);
+console.log(myarray);
+myarray.pop();
+console.log(myarray);*/
+/*var myarray=[1,2,3,4];
+alert(myarray[2]);*/
